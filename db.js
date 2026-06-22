d87b9950c29710fb863d33f40a0c5918df9b4bd4
Dnyaneshwari Mehetre <dnyaneshwarimehetre844@gmail.com>
2026-06-22 12:11:52 +0530

    Update database connection details

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ var mysql = require("mysql2");
 var util = require("util");
 
 var conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "dnyanu1234",
-  database: "dental",
+  host: "bjiregkijl0gvrbb0wf9-mysql.services.clever-cloud.com",
+  user: "u7m23vcksfx6rvbi",
+  password: "0begvLzgDiUyv7zD6meW",
+  database: "bjiregkijl0gvrbb0wf9",
 });
 var exe = util.promisify(conn.query).bind(conn);
 module.exports = exe;
